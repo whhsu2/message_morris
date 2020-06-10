8c9198ba6cd56066281f81dce60eb0a9597735fa
Ubuntu <ubuntu@ip-172-31-3-147.us-east-2.compute.internal>
2020-06-10 02:24:22 +0000

    build login and authentication

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,13 @@ $(document).on('turbolinks:load', function() {
     $('.ui.dropdown').dropdown()
 })
 
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
+})
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
