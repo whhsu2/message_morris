48a536dd41f9b64a3da8eb9b037a3cd25451460a
Ubuntu <ubuntu@ip-172-31-3-147.us-east-2.compute.internal>
2020-06-11 02:31:33 +0000

    add sign up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-    before_action :logged_in_redirect, only: [:new, :create]
+    before_action :logged_in_redirect, only: [:new, :signin, :create]
     
     
     def new
@@ -7,15 +7,27 @@ class SessionsController < ApplicationController
     end
     
     def create
-    user = User.find_by(username: params[:session][:username])
-    if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user.id
-      flash[:success] = "You have successfully logged in"
-      redirect_to root_path
-    else
-      flash.now[:error] = "There was something wrong with your login information"
-      render 'new'
+        user = User.new(username: params[:session][:username], password: params[:session][:password])
+        if user.save
+            flash[:notice] = "Welcome to chat with Morris"
+            session[:user_id] = user.id
+            redirect_to root_path
+        else
+            flash[:notice] = "User Name has been taken"
+            render 'new'
+        end
     end
+    
+    def signin
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+          session[:user_id] = user.id
+          flash[:success] = "#{user.username} have successfully logged in"
+          redirect_to root_path
+        else
+          flash.now[:error] = "There was something wrong with your login information"
+          render 'new'
+        end
     end
     
     def destroy
